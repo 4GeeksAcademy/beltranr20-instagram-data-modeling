894b7efb30eb423e002344714a734a7982cb2888
Randy Beltran <131041473+Beltranr20@users.noreply.github.com>
2023-09-03 14:23:40 +0000

    recent changes

--- src/models.py
+++ src/models.py
@@ -4,29 +4,69 @@ from sqlalchemy import Column, ForeignKey, Integer, String
 from sqlalchemy.orm import relationship, declarative_base
 from sqlalchemy import create_engine
 from eralchemy2 import render_er
+from sqlalchemy.orm import Mapped
+from sqlalchemy.orm import mapped_column
+from sqlalchemy.orm import relationship
+from typing import List
+from sqlalchemy import Table
 
 Base = declarative_base()
 
-class Person(Base):
-    __tablename__ = 'person'
+class User(Base):
+    __tablename__ = 'user'
     # Here we define columns for the table person
     # Notice that each column is also a normal Python instance attribute.
     id = Column(Integer, primary_key=True)
     name = Column(String(250), nullable=False)
 
-class Address(Base):
-    __tablename__ = 'address'
-    # Here we define columns for the table address.
+    
+class Post(Base):
+    __tablename__ = 'post'
+    # Here we define columns for the table post.
     # Notice that each column is also a normal Python instance attribute.
     id = Column(Integer, primary_key=True)
-    street_name = Column(String(250))
-    street_number = Column(String(250))
-    post_code = Column(String(250), nullable=False)
-    person_id = Column(Integer, ForeignKey('person.id'))
-    person = relationship(Person)
+    post_name = Column(String(250))
+    post_number = Column(String(250))
+    post_id = Column(Integer, ForeignKey('user.id'))
+    # comments: Mapped[List[Comments]] = relationship(secondary=association_table)
 
     def to_dict(self):
         return {}
+
+class Comments(Base):
+    __tablename__ = 'comments'
+    # Here we define columns for the table comments.
+    # Notice that each column is also a normal Python instance attribute.
+    id = Column(Integer, primary_key=True)
+    comments_context = Column(String(250))
+    comments_number = Column(String(250))
+    user_id = Column(Integer, ForeignKey('user.id'))
+    post_id = Column(Integer, ForeignKey('post.id'))
+
+    def to_dict(self):
+        return {}
+
+# class Base (declarative_base):
+#     pass
+# association_table = Table(
+#     "association_table",
+#     Base.metadata,
+#     Column("post_id", ForeignKey("post.id")),
+#     Column("comments", ForeignKey("comments.id")),
+# )
+
+class Followers (Base):
+    __tablename__ = 'followers'
+    # Here we define columns for the table comments.
+    # Notice that each column is also a normal Python instance attribute.
+    id = Column(Integer, primary_key=True)
+    followers_username = Column(String(250))
+    followers_number = Column(String(250))
+    user_id = Column(Integer, ForeignKey('user.id'))
+
+    def to_dict(self):
+        return {}
+
 
 ## Draw from SQLAlchemy base
 try:
